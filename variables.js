a0137446a1613b4c80bbd681ca19f652de219365
Cesare Gallo <cesaregallo0@gmail.com>
2021-07-30 17:23:10 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,21 @@
+Pt 2: Program Specific Challenge
+
+2. var animal = "Dog";
+
+3. var isCuddly = true;
+
+4. animal = "Cat";
+
+5. var num8 = 21;
+
+6. var num3 = 7;
+
+7. var quotient = num8/num3; //type quotient into the console (no var),press enter to get the answer= 3
+
+8. var favoriteFilms = ["The Godfather", "Clueless", "Rear Window"];
+
+9. favoriteFilms.shift(); // type favoriteFilms into the console to see the first element removed from array
+
+10. animal.length; //enter and number of characters in string will be returned
+
+//for questions 9 and 10 I was able to get the answer with and without the semi-colon, I added it to be safe :)
