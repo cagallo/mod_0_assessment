2cf083f47eb8f1dd7e9a8a8791d9e59299fb5020
Cesare Gallo <cesaregallo0@gmail.com>
2021-07-30 17:49:58 -0600

    Add house model

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var myHouse = {
+  location: "Denver, CO",
+  color: "Beige",
+  yearBuilt: 1896,
+  sizeInSquareFeet: 900,
+  isSmall: true,
+  isWellInsulated: false,
+  favoriteRooms: ["living room", "bedroom", "office"],
+  floraOnProperty: ["Daisies", "Russian Sage", "Zinnia", "Douglas Fir"],
+}
