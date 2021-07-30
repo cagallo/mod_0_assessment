2552096c84b0227421a5042e3eed3134e13034e0
Cesare Gallo <cesaregallo0@gmail.com>
2021-07-30 17:41:19 -0600

    Add laptop model

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var myMacBookPro = {
+  name: "Cesare's MacBook Pro",
+  centralProcessingUnit: "M1 Chip",
+  ageInMonths: 1,
+  sizeInInches: 13.3,
+  isFast: true,
+  isCheap: false,
+  mostUsedApplications: ['Terminal', 'Atom', 'Chrome'],
+  browsersUsed: ['Chrome', 'Safari', 'Firefox'],
+}
